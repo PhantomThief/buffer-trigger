56b81f6e17a97eca586ba087b27a024d8f3f7af1
w.vela <wangtianzhou@kuaishou.com>
2017-11-01 18:01:29 +0800

    [code healthy] junit 5.x, code cleanup.

--- src/test/java/com/github/phantomthief/test/ConflictReadWriteTest.java
+++ src/test/java/com/github/phantomthief/test/ConflictReadWriteTest.java
@@ -14,6 +14,7 @@ import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import com.github.phantomthief.collection.BufferTrigger;
@@ -27,6 +28,7 @@ class ConflictReadWriteTest {
 
     private final Map<Object, AtomicInteger> counter = new IdentityHashMap<>();
 
+    @Disabled
     @Test
     void test() {
         Random random = new Random();

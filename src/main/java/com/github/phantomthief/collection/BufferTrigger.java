c9656ffcf72ee81ce2a52cc7073fcc18d3406e94
w.vela <wangtianzhou@kuaishou.com>
2019-07-29 19:37:23 +0800

    [code style] mark deprecated for non-generic type factory method.

--- src/main/java/com/github/phantomthief/collection/BufferTrigger.java
+++ src/main/java/com/github/phantomthief/collection/BufferTrigger.java
@@ -22,15 +22,26 @@ public interface BufferTrigger<E> {
         return new GenericSimpleBufferTriggerBuilder<>(SimpleBufferTrigger.newBuilder());
     }
 
+    /**
+     * use {@link #simple()} for better generic type support.
+     */
+    @Deprecated
     static SimpleBufferTriggerBuilder<Object, Object> simpleTrigger() {
         return SimpleBufferTrigger.newBuilder();
     }
 
+    /**
+     * 提供自带背压(back-pressure)的简单批量归并消费能力
+     */
     static <E> GenericBatchConsumerTriggerBuilder<E> batchBlocking() {
         return new GenericBatchConsumerTriggerBuilder<>(
                 BatchConsumeBlockingQueueTrigger.newBuilder());
     }
 
+    /**
+     * use {@link #batchBlocking()} for better generic type support.
+     */
+    @Deprecated
     static BatchConsumerTriggerBuilder<Object> batchBlockingTrigger() {
         return BatchConsumeBlockingQueueTrigger.newBuilder();
     }

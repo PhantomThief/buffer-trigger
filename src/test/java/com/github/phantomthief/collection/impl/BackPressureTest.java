540907d202f9a82e4c854cf34ba665f2a40b360b
w.vela <wangtianzhou@kuaishou.com>
2019-07-30 20:14:01 +0800

    [test] add no block test.

--- src/test/java/com/github/phantomthief/collection/impl/BackPressureTest.java
+++ src/test/java/com/github/phantomthief/collection/impl/BackPressureTest.java
@@ -57,4 +57,33 @@ class BackPressureTest {
         cost = System.currentTimeMillis() - cost;
         assertTrue(cost >= SECONDS.toMillis(3));
     }
+
+    @Test
+    void testNoBlock() {
+        List<String> consumed = new ArrayList<>();
+        BufferTrigger<String> buffer = BufferTrigger.<String, List<String>> simple()
+                .maxBufferCount(10)
+                .interval(1, SECONDS)
+                .setContainer(() -> synchronizedList(new ArrayList<>()), List::add)
+                .consumer(it -> {
+                    logger.info("do consuming...{}", it);
+                    sleepUninterruptibly(1, SECONDS);
+                    consumed.addAll(it);
+                    logger.info("consumer done.{}", it);
+                })
+                .build();
+        long cost = System.currentTimeMillis();
+        ExecutorService executor = newFixedThreadPool(10);
+        for (int i = 0; i < 30; i++) {
+            int j = i;
+            executor.execute(() -> {
+                buffer.enqueue("" + j);
+                logger.info("enqueued:{}", j);
+            });
+        }
+        shutdownAndAwaitTermination(executor, 1, DAYS);
+        buffer.manuallyDoTrigger();
+        cost = System.currentTimeMillis() - cost;
+        assertTrue(cost <= 1200);
+    }
 }

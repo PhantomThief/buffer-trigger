a2d94f473846f4ca1e6b7d89227a4e83a286b091
w.vela <boywang004@hotmail.com>
2019-12-03 19:59:28 +0800

    [test] add test for linger.

--- src/test/java/com/github/phantomthief/test/BatchBlockDynamicTest.java
+++ src/test/java/com/github/phantomthief/test/BatchBlockDynamicTest.java
@@ -20,11 +20,10 @@ class BatchBlockDynamicTest {
 
     @Test
     void test() {
-        int[] maxSize = {100};
         Duration[] linger = {ofSeconds(1)};
         int[] consumed = {0};
         BufferTrigger<String> buffer = BufferTrigger.<String> batchBlocking()
-                .batchSize(() -> maxSize[0])
+                .batchSize(100)
                 .linger(() -> linger[0])
                 .setConsumerEx(it -> consumed[0]++)
                 .build();
@@ -41,26 +40,14 @@ class BatchBlockDynamicTest {
         sleepUninterruptibly(1, SECONDS);
         assertEquals(2, consumed[0]);
 
-        maxSize[0] = 200;
-        for (int i = 0; i < 100; i++) {
-            buffer.enqueue("test");
-        }
-        sleepUninterruptibly(20, MILLISECONDS);
-        assertEquals(2, consumed[0]);
-        for (int i = 0; i < 100; i++) {
-            buffer.enqueue("test");
-        }
-        sleepUninterruptibly(20, MILLISECONDS);
-        assertEquals(3, consumed[0]);
-
         linger[0] = ofSeconds(2);
         sleepUninterruptibly(1, SECONDS);
         for (int i = 0; i < 10; i++) {
             buffer.enqueue("test");
         }
         sleepUninterruptibly(1, SECONDS);
-        assertEquals(4, consumed[0]);
+        assertEquals(2, consumed[0]);
         sleepUninterruptibly(1, SECONDS);
-        assertEquals(5, consumed[0]);
+        assertEquals(3, consumed[0]);
     }
 }
